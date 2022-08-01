02b1cf5298bc382506b985131707628ace254b79
lcy668 <liuchaoyanzi1103@163.com>
2022-08-01 16:49:44 +0800

    chore: 配置了路径别名

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,14 +2,25 @@
   <h1>首页</h1>
   {{ $store.state.count }}
   {{ store.state.count }}
+  <img
+    src="@/assets/vue.svg"
+    alt="logo"
+  >
+  <div class="csa" />
 </template>
 
 <script lang='ts' setup>
-import { useStore } from '../../store'
+import { useStore } from '@/store'
 
 const store = useStore()
 
 console.log(store.state.count)
 </script>
 
-<style lang='less' scoped></style>
+<style lang='less' scoped>
+.csa{
+  width: 200px;
+  height: 200px;
+  background: url('@/assets/vue.svg') no-repeat;
+}
+</style>
